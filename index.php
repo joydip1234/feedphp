ddf83600dbf1b6a0aff3906d6fe4262b6cd441bd
Sudipta <sudipta.banik.112@gmail.com>
2017-03-30 10:38:31 +0530

    made better

--- index.php
+++ index.php
@@ -1,14 +1,11 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Sudipta
- * Date: 3/29/2017
- * Time: 7:53 AM
- */
-?>
+
 <html>
-<head><title>Text Feedback saver</title>
+<head>
+<meta content="" charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0 ,user-scalable=no">
+<title>Feedback Form</title>
     <style>
+		@import url('https://fonts.googleapis.com/css?family=Droid+Sans');
         body {
             margin: 0;
             padding: 0;
@@ -24,7 +21,7 @@
             width: 80vw;
             margin: 10vh auto;
             padding: 20px;
-            top:20vh;
+            top:4vh;
             left: 10vw;
             box-sizing: border-box;
             background-color: rgba(34, 56, 67, 0.9);
@@ -39,15 +36,18 @@
             box-sizing: border-box;
             border: none;
             outline: none;
-            background-color: #333;
+            background-color: transparent;
             color: white;
             margin: 3px auto;
             font-family: "Droid Sans", sans-serif;
             transition: .25s;
-            border-radius: 2px;
-            box-shadow: 0 2px 2px 0 rgba(0, 0, 0, 0.14), 0 1px 5px 0 rgba(0, 0, 0, 0.12), 0 3px 1px -2px rgba(0, 0, 0, 0.2);
+            /*box-shadow: 0 2px 2px 0 rgba(0, 0, 0, 0.14), 0 1px 5px 0 rgba(0, 0, 0, 0.12), 0 3px 1px -2px rgba(0, 0, 0, 0.2);*/
+			border-bottom:1px solid rgba(200,200,200,0.3);
         }
-
+		.input:focus{
+			border-bottom:1px solid rgba(200,200,200,1);
+		}
+		
         .button {
             padding: 7px 15px;
             border: none;
@@ -88,6 +88,33 @@
             transition: 0.3s;
             animation: fromTop 0.2s ease-in;
         }
+		.welcome_msg{
+			display:block;
+			width:100%;
+			padding:5vh;
+			box-sizing:border-box;
+			font-family:inherit;
+			font-size:1.2em;
+			font-weight:600;
+			color:#efefef;
+			background-color:rgba(0,0,0,0.12);
+		}
+		@media(max-width:700px){
+			#form {
+				display: block;
+				position: fixed;
+				width: 100vw;
+				margin:0;
+				padding: 20px;
+				top:0;
+				left:0;
+				height:100vh;
+				box-sizing: border-box;
+				background-color: rgba(34, 56, 67, 0.9);
+				color: white;
+			}
+
+		}
 
     </style>
 </head>
@@ -96,13 +123,21 @@
 <div id="notification">
 
 </div>
+<!--?php
+ if(!isset($_COOKIE[crc32('vis')])){
+	setcookie(crc32('vis'),0,time() + (86400 * 30),"/");
+	echo "<h3 class='welcome_msg'>Hello new Visitor</h3>";
+ }else{
+	echo "<h3 class='welcome_msg'>Hello Again</h3>";
+ }
+?-->
 
 <!--<form id="form" action="resp.php" method="post">-->
 <div id="form">
 
     <input class="input" type="text" name="name" id="name" placeholder="name" autocomplete="off"/><br>
     <input class="input" type="email" name="mail" id="mail" placeholder="mail" autocomplete="off"/><br>
-    <textarea class="input" placeholder="your feedback" name="feed" id="feed" style="resize: none"></textarea><br>
+    <textarea class="input" placeholder="your feedback" name="feed" id="feed" style="resize: none;min-height:30vh;"></textarea><br>
     <button class="button" type="submit" id="submitBtn">submit</button>
     <span style="clear: both;display: block"></span>
 
